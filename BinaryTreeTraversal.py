a521981e9530894441c7cd0060b613b7405f075c
yansong li <857367901@qq.com>
2015-02-08 19:59:48 -0500

    Add revised version

--- BinaryTreeTraversal.py
+++ BinaryTreeTraversal.py
@@ -32,3 +32,36 @@ class Solution:
         # note: add the last elements in currentLevelNodes
         result.append(currentLevelNodes)
         return result
+
+
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        solution = []
+
+        if root == None:
+            return solution
+
+        toProcess = [root]
+
+        while len(toProcess) != 0:
+            vals = []
+            nextLevel = []
+            for n in toProcess:
+                vals.append(n.val)
+                if n.left != None:
+                    nextLevel.append(n.left)
+                if n.right != None:
+                    nextLevel.append(n.right)
+            solution.append(vals)
+            toProcess = nextLevel
+        return solution
