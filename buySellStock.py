9a02568820d6befa5ddd1bde44484f333b0cbc59
yansong li <857367901@qq.com>
2015-02-10 09:46:11 -0500

    buySellStock

---
+++ buySellStock.py
@@ -0,0 +1,35 @@
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        n = len(prices)
+        if n <= 1:
+            return 0
+        low = 0
+        high = 0
+
+        currentMax = 0
+        result = []
+        for i in range(n):
+            if prices[i] >= prices[high]:
+                high = i
+                print 'low is : '+ str(low)
+                print 'high is : '+ str(high)
+                currentMax = prices[high] - prices[low]
+            if prices[i] < prices[low]:
+                result.append(currentMax)
+                low = i
+                high = i
+                print 'low is : '+ str(low)
+                print 'high is : '+ str(high)
+                currentMax = 0
+
+        result.append(currentMax)
+
+        result.sort()
+        return result[-1]
+
+if __name__ == '__main__':
+    s = Solution()
+    m = s.maxProfit([4,1,2])
+    print m
