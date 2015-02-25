6f32288de15bcd6f364fef9ab2a256b99e596c8b
yansong li <857367901@qq.com>
2015-02-25 17:12:58 +0800

    Add LRU

--- LRUCache.py
+++ LRUCache.py
@@ -1,3 +1,72 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.val = val
+        self.key = key
+        self.next = None
+        self.prev = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, node):
+        if self.head is None:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+        self.tail = node
+
+    def delete(self, node):
+        if node.prev:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+        if node.next:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+        del node
+
+class LRUCache:
+
+    # @param capacity, an integer
+    def __init__(self, capacity):
+        self.list = LinkedList()
+        self.dict = {}
+        self.capacity = capacity
+
+    def _insert(self, key, val):
+        node = ListNode(key, val)
+        self.list.insert(node)
+        self.dict[key] = node
+
+
+    # @return an integer
+    def get(self, key):
+        if key in self.dict:
+            val = self.dict[key].val
+            self.list.delete(self.dict[key])
+            self._insert(key, val)
+            return val
+        return -1
+
+
+    # @param key, an integer
+    # @param value, an integer
+    # @return nothing
+    def set(self, key, val):
+        if key in self.dict:
+            self.list.delete(self.dict[key])
+        elif len(self.dict) == self.capacity:
+            del self.dict[self.list.head.key]
+            self.list.delete(self.list.head)
+        self._insert(key, val)
+
+
+
+
 class LRUCache:
 
     # @param capacity, an integer
@@ -65,9 +134,6 @@ class LRUCache:
         else:
             self.head = self.head.next
             self.head.pre = None
-
-
-10,[set(10,13),set(3,17),set(6,11),set(10,5),set(9,10),get(13),set(2,19),get(2),get(3),set(5,25),get(8),set(9,22),set(5,5),set(1,30),get(11),set(9,12),get(7),get(5),get(8),get(9),set(4,30),set(9,3),get(9),get(10),get(10),set(6,14),set(3,1),get(3),set(10,11),get(8),set(2,14),get(1),get(5),get(4),set(11,4),set(12,24),set(5,18),get(13),set(7,23),get(8),get(12),set(3,27),set(2,12),get(5),set(2,9),set(13,4),set(8,18),set(1,7),get(6),set(9,29),set(8,21),get(5),set(6,30),set(1,12),get(10),set(4,15),set(7,22),set(11,26),set(8,17),set(9,29),get(5),set(3,4),set(11,30),get(12),set(4,29),get(3),get(9),get(6),set(3,4),get(1),get(10),set(3,29),set(10,28),set(1,20),set(11,13),get(3),set(3,12),set(3,8),set(10,9),set(3,26),get(8),get(7),get(5),set(13,17),set(2,27),set(11,15),get(12),set(9,19),set(2,15),set(3,16),get(1),set(12,17),set(9,1),set(6,19),get(4),get(5),get(5),set(8,1),set(11,7),set(5,2),set(9,28),get(1),set(2,2),set(7,4),set(4,22),set(7,24),set(9,26),set(13,28),set(11,26)
 
     def insert_to_tail(self, node):
 
